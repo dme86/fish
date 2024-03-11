731b66a91f950bc7a57d0e6fcf535a6c29fdf73b
Dan <me.daniel.meier@gmail.com>
2024-03-11 14:08:06 +0100

    feat(add): passgrep
    Adding 'passgrep' to fish shell. It allows an easy grep of 'pass' arguments like 'url' or 'user' etc.

--- config.fish
+++ config.fish
@@ -17,6 +17,24 @@ alias vim nvim
 fish_add_path /opt/local/bin
 set -gx GPG_TTY (tty)
 
+function passgrep
+    set entry $argv[1]
+    set part $argv[2]
+
+    # Use pass command to retrieve the password, grep for the specified part, and format the output
+    set result (pass $entry | grep $part | cut -d ':' -f 2,3 | awk '{$1=$1;print}')
+
+    # Check if the result is empty
+    if test -n "$result"
+        # Copy the result to the clipboard
+        echo $result | xclip -sel clip
+        echo "$part for '$entry' copied to clipboard."
+    else
+        # Print a message indicating that the entry or part was not found
+        echo "Error: '$entry' or '$part' not found."
+    end
+end
+
 function ..
     cd ..
 end
